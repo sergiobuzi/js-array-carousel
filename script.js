3701b4959be94d38d5e730d85661b510ba40575d
SergioBuzi <sergio.buzi97@gmail.com>
2023-11-23 16:31:00 +0100

    config: button next

---
+++ script.js
@@ -0,0 +1,35 @@
+//creo la costante image e la stampo
+const image = document.getElementsByClassName('img-container');
+console.log(image);
+
+//creo una flag e la imposto su 0
+let activeItem = 0;
+
+//configuro il bottone per andare avanti
+const next = document.querySelector('.bottom');
+
+next.addEventListener('click',
+
+    function(){
+
+        if (activeItem < image.length - 1) {
+            //tolgo la classe active all'immagine
+            image[activeItem].classList.remove('active');
+
+            //aumento l'indice dell'elemento da visualizzare
+            activeItem++;
+
+            //aggiungo la classe active all'elemento successivo
+            image[activeItem].classList.add('active');
+
+
+            if (activeItem === image.length - 1) {
+
+                //arrivo all'ultima immagine
+                next.classList.add('hidden');
+            }
+        }
+
+    }
+
+);
