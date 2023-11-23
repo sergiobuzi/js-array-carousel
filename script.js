d806721b010f64e7d1e2b3e392cdfbb153804a3c
SergioBuzi <sergio.buzi97@gmail.com>
2023-11-23 16:50:24 +0100

    config: back button

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log(image);
 //creo una flag e la imposto su 0
 let activeItem = 0;
 
-//configuro il bottone per andare avanti
+//configuro il bottone next per andare avanti
 const next = document.querySelector('.bottom');
 
 next.addEventListener('click',
@@ -13,6 +13,7 @@ next.addEventListener('click',
     function(){
 
         if (activeItem < image.length - 1) {
+
             //tolgo la classe active all'immagine
             image[activeItem].classList.remove('active');
 
@@ -28,8 +29,39 @@ next.addEventListener('click',
                 //arrivo all'ultima immagine
                 next.classList.add('hidden');
             }
+
         }
 
     }
 
+);
+
+
+//configuro il bottone back per andare indietro
+
+const back = document.querySelector('top');
+
+back.addEventListener('click',
+
+    function(){
+
+        if (activeItem < image.lenght - 1) {
+
+            //tolgo la classe active dall'immagine
+            image[activeItem].classList.add('active');
+
+            //aumento l'indice dell'elemento da visualizzare
+            activeItem--;
+
+            //aggiungo la classe active all'elemento successivo
+            image[activeItem].classList.remove('active');
+
+
+            if (activeItem === 0) {
+
+                //arrivo all'ultima immagine
+                back.classList.add('hidden');
+            }
+        }
+    }
 );
